59429690a13ec69c3c27aa1bc4e69aaf9458fe5c
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-06-10 19:56:54 +0300

    feat: getUser endpointi yazıldı, verileri manipüle edildi ve test edildi

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,12 +2,29 @@ const User = require('../models/user');
 
 exports.getUser = async (req, res) => {
     try {
-        const user = await User.findOne({$or:[{phone: req.body.phone}, {email: req.body.email}]});
-        if(!user) {
+        const user = await User.findOne({ $or: [{ phone: req.body.phone }, { email: req.body.email }] });
+        if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
-        res.status(201).json(user);
+        const { birthday, name, email, phone, insta, tiktok, money, job, country, city, gender } = user;
+        const userData = {
+            birthday,
+            name,
+            email,
+            phone,
+            instagram: insta,
+            tiktok,
+            money,
+            job,
+            country,
+            city,
+            gender
+        };
+
+        res.status(201).json(userData);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
-    };
+};
+
+    // doğumgünü, isim, email, telefon, instagram, tiktok, money, job, country/city, gender
